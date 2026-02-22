66265ed067fa99712f4fc3067f319fa06ec22750
Subhransu Sekhar Mishra <msubhransu1@gmail.com>
2026-02-22 20:24:37 +0530

    Setup and installations

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+function App() {
+
+  return (
+    <>
+     <h1>Whiteboard AI</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Singnup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Singnup = () => {
+  return (
+    <div>
+      <h1>Singnup</h1>
+    </div>
+  )
+}
+
+export default Singnup
